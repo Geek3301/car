799cf682384dfd0bb830c3db6e2979c128ece007
Fedir <47052998+Geek3301@users.noreply.github.com>
2024-09-02 18:25:32 +0200

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1725294258,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/wall.png",
+		"images/tiledbackground.png",
+		"images/car-sheet0.png",
+		"images/target-sheet0.png",
+		"images/red-sheet0.png",
+		"images/green-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
